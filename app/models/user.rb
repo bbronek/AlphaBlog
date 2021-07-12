a070adba1f84f8f453eaa3afbda181e9e0381a58
bbronek <barbro214@gmail.com>
2021-07-12 03:56:10 +0200

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
                       length: {minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  
+
   validates :email, presence: true,
                      uniqueness: {case_sensitive: false}, 
-                     length: {maximum: 105}
+                     length: {maximum: 105},
+                     foramt: {with: VALID_EMAIL_REGEX}
 
 end
